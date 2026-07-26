e19ec9c6569110da48a1d26e3dd86d3515d3fe28
Łukasz Lenart <lenartluk@gmail.com>
2026-07-26 21:43:40 +0200

    refactor(scraper): change typings

--- backend/app/services/scraper/base.py
+++ backend/app/services/scraper/base.py
@@ -9,14 +9,15 @@ from app.schemas.search import SimpleScrapedFlight
 class BaseScraper(ABC):
 
     @abstractmethod
-    async def fetch_flights(self, origin: str, destination: str, date_from: date, date_to: date) -> dict:
+    async def fetch_flights(self, origin: str, destination: str, date_from: date, date_to: date | None) -> dict | None:
         """Fetches flights for given route and date"""
 
         pass
 
 
+    @staticmethod
     @abstractmethod
-    def parse_response(response: dict) -> list[SimpleScrapedFlight]:
+    def parse_response(response: dict | None) -> list[SimpleScrapedFlight]:
         """Parses fetched flight response"""
         
         pass
