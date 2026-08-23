8de00f5d4a48f1cbdfa9b9e7832b4cc25292eaf0
Łukasz Lenart <lenartluk@gmail.com>
2026-08-23 15:34:30 +0200

    bug(date_helpers): change date chunk logic

--- backend/app/utils/date_helpers.py
+++ backend/app/utils/date_helpers.py
@@ -3,33 +3,27 @@ from datetime import timedelta, date
 
 
 def merge_date_chunks(dep_chunk: list[date], arr_chunk: list[date]) -> list[tuple[date, date | None]]:
-
     chunks = []
     max_len = max(len(dep_chunk), len(arr_chunk))
 
     for i in range(max_len):
         dep = dep_chunk[i] if i < len(dep_chunk) else dep_chunk[-1]
         arr = arr_chunk[i] if i < len(arr_chunk) else arr_chunk[-1]
-
-        if arr is not None and arr < dep:
-            chunks.append((arr, arr))
-        else:
-            chunks.append((dep, arr))
+        chunks.append((dep, arr))
 
     return chunks
 
 
-def get_date_chunks(date_from: date, date_to: date, window_days: int = 7) -> list[date | None]:
-    print(f"takie to jest {date_from} {date_to}")
+def get_date_chunks(date_from: date, date_to: date, window_days: int = 5) -> list[date | None]:
     if date_from is None or date_to is None:
         return [None]
-    
-    date_chunks = []
 
-    current_date = date_from 
-    while current_date <= date_to:
-        date_chunks.append(current_date)
-        current_date += timedelta(days=window_days)
+    date_chunks = []
+    range_date = date_from + timedelta(days=2)
+
+    while range_date - timedelta(days=2) <= date_to:
+        date_chunks.append(range_date)
+        range_date += timedelta(days=window_days)
 
     return date_chunks
 
@@ -60,6 +54,7 @@ def compute_weekend_dates(date_from: date, date_to: date) -> list[tuple[date, da
         temp_date = date_from + timedelta(days=(4 - date_from.weekday()))
         weekend_dates.append((temp_date, temp_date))
 
+    # setting date to Friday
     to_add = 6 - date_from.weekday() + 5
     current_date = date_from + timedelta(days=to_add)
 
